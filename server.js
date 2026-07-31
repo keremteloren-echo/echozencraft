c7c3608e0ab3f0cc58e39b11f58f65af3c8e5546
Kerem Teloren <keremteloren@gmail.com>
2026-07-31 10:25:44 +0300

    fix: complete puppeteer setup and auto-find browser

--- server.js
+++ server.js
@@ -265,11 +265,8 @@ async function generateAnalysisPDF(data) {
 </body>
 </html>`;
 
-  const execPath = await puppeteer.executablePath();
-
   const browser = await puppeteer.launch({
     headless: 'new',
-    executablePath: execPath,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
